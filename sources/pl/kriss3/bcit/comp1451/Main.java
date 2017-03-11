35ef92a2fe92b5026550870c7107a3aa2841f42f
Kris <kris_s2@outlook.com>
2017-03-11 13:10:16 -0800

    Initial commit to Assignment 02 BCIT Class Comp 1451. Java 2

---
+++ sources/pl/kriss3/bcit/comp1451/Main.java
@@ -0,0 +1,15 @@
+package pl.kriss3.bcit.comp1451;
+
+/**
+ * Starting point of Assignment 02
+ * @author Krzysztof Szczurowski
+ * @see https://github.com/kriss3/BCIT_JavaSemester02-COMP1451_Assignment02.git
+ * @ 2017-03-11 (set as static)
+ */
+public class Main 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Assignment 02 Start ...");
+	}
+}
